54c069387836f77cc76773b93b1e866a1f39b9ac
eswagel <73608467+eswagel@users.noreply.github.com>
2025-12-26 16:03:18 -0500

    Add to Detangle.jl

--- src/Detangle.jl
+++ src/Detangle.jl
@@ -1,5 +1,14 @@
 module Detangle
 
-# Write your package code here.
+include("effects.jl")
+include("regions.jl")
+include("access.jl")
+include("task.jl")
+
+export Read, Write, ReadWrite, Reduce
+export Whole, Key, Block, Tile, IndexSet
+export Access, TaskSpec, access, objkey, add_access!
+
+# TODO: include and export remaining modules (conflicts, dag, scheduler, macros, diagnostics, reduce_priv) as they are implemented.
 
 end
